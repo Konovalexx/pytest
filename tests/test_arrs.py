e1de781bb6932c26b0dda58c06838998f201f1dc
Alex Con <konovalexx@gmail.com>
2024-03-25 20:31:30 +1000

    Index test

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -1,8 +1,14 @@
 from utils import arrs
 
 
+# В данном случае тест не проходил из-за того, что не учтено то, что в языке питон индексы начинаются с 0. Т.е.
+# элемент с индексом 1 в списке [1, 2, 3] это 2, а не 3.
+# def test_get():
+#     assert arrs.get([1, 2, 3], 1, "test") == 3
+#     assert arrs.get([], 0, "test") == "test"
+
 def test_get():
-    assert arrs.get([1, 2, 3], 1, "test") == 3
+    assert arrs.get([1, 2, 3], 1, "test") == 2
     assert arrs.get([], 0, "test") == "test"
 
 
